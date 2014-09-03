df18b41be1b1f9d69b9853c1753d6e8b06bde6b4
codewithpassion <dominik.fretz@gmail.com>
2014-09-03 20:42:44 +1000

    rename to software-upload

--- src/plugins/09_software-upload/public/js/software.js
+++ src/plugins/09_software-upload/public/js/software.js
@@ -26,20 +26,20 @@
 
     var softwareViewModel = { visible: ko.observable(false) };
     // Add required UI elements
-    $('#sidebar').append('<div id="software-sidebar" data-bind="template: { name: \'sidebarElementTemplate\' }"></div>');
+    $('#sidebar').append('<div id="softwareupdate-sidebar" data-bind="template: { name: \'sidebarElementTemplate\' }"></div>');
 
-    var sideBarViewModel = { description: 'Software', click: function(){
+    var sideBarViewModel = { description: 'Software install', click: function(){
       softwareViewModel.visible(!softwareViewModel.visible());
     }};
-    ko.applyBindings(sideBarViewModel , $('#software-sidebar')[0]);
+    ko.applyBindings(sideBarViewModel , $('#softwareupdate-sidebar')[0]);
 
-    $('#main-row').append('<div id="software" data-bind="visible: visible"><div id="software-content"></div></div>');
+    $('#main-row').append('<div id="softwareupdate" data-bind="visible: visible"><div id="softwareupdate-content"></div></div>');
 
-    ko.applyBindings(softwareViewModel, $('#software')[0]);
-    $('#software-content').load('plugin/09_software-update/plugin.html', function () {
-      ko.applyBindings(vm, $('#software')[0]);
+    ko.applyBindings(softwareViewModel, $('#softwareupdate')[0]);
+    $('#softwareupdate-content').load('plugin/09_software-upload/plugin.html', function () {
+      ko.applyBindings(vm, $('#softwareupdate')[0]);
     });
-    console.log('Loaded Software plugin.');
+    console.log('Loaded Software install plugin.');
   };
   window.Dashboard.plugins.push(Software);
 }(window, jQuery));
