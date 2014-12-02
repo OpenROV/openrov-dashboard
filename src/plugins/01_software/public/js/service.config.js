cab57e1217df92212f9ed7eddb5e23747d7cd0a7
codewithpassion <dominik.fretz@gmail.com>
2014-12-02 17:51:39 +1100

    Adding frontend test harness

--- src/plugins/01_software/public/js/service.config.js
+++ src/plugins/01_software/public/js/service.config.js
@@ -1,3 +1,21 @@
-var ConfigService = [function() {
+var ConfigService = ['$q', '$http', function($q, $http) {
+  var config = {};
 
+  config.isUpdateEnabled = function() {
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      url: 'plugin/software/config'
+    })
+      .then(
+      function(result) {
+        deferred.resolve(result.data.enableUpdates);
+      },
+      function() {
+        deferred.resolve(false);
+      });
+    return deferred.promise;
+  };
+
+  return config;
 }];
---
+++ src/plugins/01_software/public/tests/service.config.test.js
@@ -0,0 +1,8 @@
+describe('service.config', function() {
+
+
+
+  it('The example plugin should be loaded', function() {
+    true.should.be.true;
+  });
+});
